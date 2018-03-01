6f537c30cc41ed22c42ea6248f8e2d149281a73e
Christopher Speller <crspeller@gmail.com>
2018-03-01 05:02:56 -0800

    Basic plugin setup

--- server/config.go
+++ server/config.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 type Configuration struct {
+	GithubToken string
 }
 
 func (c *Configuration) IsValid() error {
+	if c.GithubToken == "" {
+		return fmt.Errorf("Must have a github token")
+	}
+
 	return nil
 }
--- server/plugin.go
+++ server/plugin.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"strings"
 	"sync/atomic"
 
+	"github.com/google/go-github/github"
+	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
+	"golang.org/x/oauth2"
 )
 
 type Plugin struct {
 	api           plugin.API
 	configuration atomic.Value
+	githubClient  *github.Client
+}
+
+func githubConnect(token string) *github.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	return client
 }
 
 func (p *Plugin) OnActivate(api plugin.API) error {
@@ -23,7 +41,53 @@ func (p *Plugin) OnActivate(api plugin.API) error {
 		return err
 	}
 
+	// Connect to github
+	p.githubClient = githubConnect(config.GithubToken)
+
+	// Register commands
+	p.api.RegisterCommand(&model.Command{
+		Trigger:     "github",
+		DisplayName: "Github",
+		Description: "Integration with Github.",
+	})
+
 	return nil
+}
+
+func (p *Plugin) ExecuteCommand(args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
+	split := strings.Split(args.Command, " ")
+	command := split[0]
+	parameters := []string{}
+	action := ""
+	if len(split) > 1 {
+		action = split[1]
+	}
+	if len(split) > 2 {
+		parameters = split[2:]
+	}
+
+	if command != "/github" {
+		return nil, nil
+	}
+
+	switch action {
+	case "subscribe":
+		if len(parameters) != 1 {
+			return nil, nil
+		}
+	case "register":
+	case "todo":
+	}
+
+	resp := &model.CommandResponse{
+		ResponseType: model.COMMAND_RESPONSE_TYPE_IN_CHANNEL,
+		Text:         "You have subscribed to the repository.",
+		Username:     "github",
+		IconURL:      "https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png",
+		Type:         model.POST_DEFAULT,
+	}
+
+	return resp, nil
 }
 
 func (p *Plugin) config() *Configuration {
