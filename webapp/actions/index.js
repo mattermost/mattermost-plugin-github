c305f4c69e75390621fb9031bfb50ea791edb040
JoramWilander <jwawilander@gmail.com>
2018-03-02 00:59:40 +0000

    Add pull request post component

--- webapp/actions/index.js
+++ webapp/actions/index.js
@@ -1,3 +1,35 @@
 import {PostTypes} from 'mattermost-redux/action_types';
+import {getPost} from 'mattermost-redux/selectors/entities/posts';
 
 import Client from '../client';
+
+export function requestReviewers(postId, prId, reviewers, org, repo) {
+    return async (dispatch, getState) => {
+        const post = getPost(getState(), postId);
+
+        try {
+            await Client.requestReviewers(prId, reviewers, org, repo);
+        } catch (error) {
+            return {error};
+        }
+
+        if (!post) {
+            return {data: true};
+        }
+
+        const props = {...(post.props || {}), reviewers};
+
+        dispatch({
+            type: PostTypes.RECEIVED_POSTS,
+            data: {
+                order: [],
+                posts: {
+                    [post.id]: {...post, props}
+                }
+            },
+            channelId: post.channel_id
+        });
+
+        return {data: true};
+    };
+}
--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,11 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
+
+import PostTypePullRequest from './components/post_type_pull_request';
+
 class PluginClass {
     initialize(registerComponents, store) {
+        registerComponents({}, {custom_github_pull_request: PostTypePullRequest});
     }
 }
 
