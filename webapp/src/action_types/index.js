f29beb95d550feae0d323846d06f145594738e2a
marianunez <maria.nunez@mattermost.com>
2019-06-27 14:02:41 -0400

    Adding missing file in previous commit."

--- webapp/src/action_types/index.js
+++ webapp/src/action_types/index.js
@@ -1,13 +1,13 @@
 import {id as pluginId} from '../manifest';
 
 export default {
-    RECEIVED_REVIEWS: pluginId + 'received_reviews',
-    RECEIVED_YOUR_PRS: pluginId + 'received_your_prs',
-    RECEIVED_YOUR_ASSIGNMENTS: pluginId + 'received_your_assignments',
-    RECEIVED_MENTIONS: pluginId + 'received_mentions',
-    RECEIVED_UNREADS: pluginId + 'received_unreads',
-    RECEIVED_CONNECTED: pluginId + 'received_connected',
-    RECEIVED_GITHUB_USER: pluginId + 'received_github_user',
-    RECEIVED_RHS_ACTION: pluginId + 'received_rhs_action',
+    RECEIVED_REVIEWS: pluginId + '_received_reviews',
+    RECEIVED_YOUR_PRS: pluginId + '_received_your_prs',
+    RECEIVED_YOUR_ASSIGNMENTS: pluginId + '_received_your_assignments',
+    RECEIVED_MENTIONS: pluginId + '_received_mentions',
+    RECEIVED_UNREADS: pluginId + '_received_unreads',
+    RECEIVED_CONNECTED: pluginId + '_received_connected',
+    RECEIVED_GITHUB_USER: pluginId + '_received_github_user',
+    RECEIVED_RHS_ACTION: pluginId + '_received_rhs_action',
     UPDATE_RHS_STATE: pluginId + '_update_rhs_state',
 };
