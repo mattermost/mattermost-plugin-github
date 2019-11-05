eaa3849d0b105d5adfd68fb8f391fc34ac8c3fc7
Gabriel Linden Sagula <gsagula@gmail.com>
2019-11-05 15:59:59 -0800

    MM-18722: initial commit
    Signed-off-by: Gabriel Linden Sagula <gsagula@gmail.com>

--- server/subscriptions.go
+++ server/subscriptions.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/mattermost/mattermost-server/mlog"
+	"github.com/mattermost/mattermost-server/utils"
 
 	"github.com/google/go-github/v25/github"
 )
@@ -147,38 +150,34 @@ func (p *Plugin) GetSubscriptionsByChannel(channelID string) ([]*Subscription, e
 	return filteredSubs, nil
 }
 
-func (p *Plugin) AddSubscription(repo string, sub *Subscription) error {
-	subs, err := p.GetSubscriptions()
-	if err != nil {
-		return err
-	}
-
-	repoSubs := subs.Repositories[repo]
-	if repoSubs == nil {
-		repoSubs = []*Subscription{sub}
-	} else {
-		exists := false
-		for index, s := range repoSubs {
-			if s.ChannelID == sub.ChannelID {
-				repoSubs[index] = sub
-				exists = true
-				break
+// AddSubscription ...
+func (p *Plugin) AddSubscription(repository string, subscription *Subscription) error {
+	return p.Helpers.KVAtomicModify(context.Background(), SUBSCRIPTIONS_KEY, utils.NewTokenBucket(20*time.Millisecond, 3), func(initial []byte) ([]byte, error) {
+		var subs *Subscriptions
+		if initial == nil {
+			subs = &Subscriptions{Repositories: map[string][]*Subscription{}}
+		} else {
+			json.NewDecoder(bytes.NewReader(initial)).Decode(&subs)
+		}
+		repoSubs := subs.Repositories[repository]
+		if repoSubs == nil {
+			repoSubs = []*Subscription{subscription}
+		} else {
+			exists := false
+			for index, s := range repoSubs {
+				if s.ChannelID == subscription.ChannelID {
+					repoSubs[index] = subscription
+					exists = true
+					break
+				}
+			}
+			if !exists {
+				repoSubs = append(repoSubs, subscription)
 			}
 		}
-
-		if !exists {
-			repoSubs = append(repoSubs, sub)
-		}
-	}
-
-	subs.Repositories[repo] = repoSubs
-
-	err = p.StoreSubscriptions(subs)
-	if err != nil {
-		return err
-	}
-
-	return nil
+		subs.Repositories[repository] = repoSubs
+		return json.Marshal(repoSubs)
+	})
 }
 
 func (p *Plugin) GetSubscriptions() (*Subscriptions, error) {
@@ -196,15 +195,6 @@ func (p *Plugin) GetSubscriptions() (*Subscriptions, error) {
 	}
 
 	return subscriptions, nil
-}
-
-func (p *Plugin) StoreSubscriptions(s *Subscriptions) error {
-	b, err := json.Marshal(s)
-	if err != nil {
-		return err
-	}
-	p.API.KVSet(SUBSCRIPTIONS_KEY, b)
-	return nil
 }
 
 func (p *Plugin) GetSubscribedChannelsForRepository(repo *github.Repository) []*Subscription {
@@ -243,39 +233,37 @@ func (p *Plugin) GetSubscribedChannelsForRepository(repo *github.Repository) []*
 	return subsToReturn
 }
 
-func (p *Plugin) Unsubscribe(channelID string, repo string) error {
+// Unsubscribe ...
+func (p *Plugin) Unsubscribe(channelID string, repository string) error {
 	config := p.getConfiguration()
-
-	repo, _, _ = parseOwnerAndRepo(repo, config.EnterpriseBaseURL)
-
-	if repo == "" {
-		return fmt.Errorf("Invalid repository")
+	repository, _, _ = parseOwnerAndRepo(repository, config.EnterpriseBaseURL)
+	if repository == "" {
+		return errors.New("invalid repository")
 	}
+	bucket := utils.NewTokenBucket(20*time.Millisecond, 3)
+	ctx := context.Background()
+	err := p.Helpers.KVAtomicModify(ctx, SUBSCRIPTIONS_KEY, bucket, func(initial []byte) ([]byte, error) {
+		if initial == nil {
+			return nil, errors.New("gh.plugin.err: nothing to be done")
+		}
+		var subs *Subscriptions
+		json.NewDecoder(bytes.NewReader(initial)).Decode(&subs)
+		repoSubs := subs.Repositories[repository]
+		if repoSubs == nil {
+			return nil, errors.New("gh.plugin.err: nothing to be done")
+		}
+		for index, sub := range repoSubs {
+			if sub.ChannelID == channelID {
+				repoSubs = append(repoSubs[:index], repoSubs[index+1:]...)
+				subs.Repositories[repository] = repoSubs
+				return json.Marshal(subs)
+			}
+		}
+		return nil, errors.New("gh.plugin.err: nothing to be done")
+	})
 
-	subs, err := p.GetSubscriptions()
-	if err != nil {
+	if err.Error() != "gh.plugin.err: nothing to be done" {
 		return err
-	}
-
-	repoSubs := subs.Repositories[repo]
-	if repoSubs == nil {
-		return nil
-	}
-
-	removed := false
-	for index, sub := range repoSubs {
-		if sub.ChannelID == channelID {
-			repoSubs = append(repoSubs[:index], repoSubs[index+1:]...)
-			removed = true
-			break
-		}
-	}
-
-	if removed {
-		subs.Repositories[repo] = repoSubs
-		if err := p.StoreSubscriptions(subs); err != nil {
-			return err
-		}
 	}
 
 	return nil
