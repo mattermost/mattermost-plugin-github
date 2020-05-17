c155d647adfbde0a50693b82f294cdd4f06db9f9
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2020-05-17 09:06:14 -0300

    run linter

--- webapp/src/components/link_tooltip/index.jsx
+++ webapp/src/components/link_tooltip/index.jsx
@@ -2,11 +2,13 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
+
 import {id as pluginId} from 'manifest';
+
 import {LinkTooltip} from './link_tooltip';
 
-const mapStateToProps = (state, ownProps) => {
-    return { connected: state[`plugins-${pluginId}`].connected }
+const mapStateToProps = (state) => {
+    return {connected: state[`plugins-${pluginId}`].connected};
 };
 
 export default connect(mapStateToProps, null)(LinkTooltip);
