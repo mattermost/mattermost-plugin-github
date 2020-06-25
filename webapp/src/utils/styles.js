97e077d9c7067cbc6314d5cd3b9717f71c9bbe44
Federico Martín Alconada Verzini <federico.alconada@icloud.com>
2020-06-25 15:22:32 -0300

    [GH-294] link user's theme to tooltip (#303)
    Co-authored-by: Federico Martín Alconada Verzini <fedealconada@gmail.com>
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- webapp/src/utils/styles.js
+++ webapp/src/utils/styles.js
@@ -109,3 +109,13 @@ export const getLabelFontColor = (hexcolor) => {
     const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
     return (yiq >= 128) ? 'black' : 'white';
 };
+
+export const hexToRGB = (hex, alpha) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    if (alpha) {
+        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+    }
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+};
