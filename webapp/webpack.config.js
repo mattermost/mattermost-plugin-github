1af30dc0aaf4e2edde3c4a31c50496dca1972283
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-08-04 01:10:44 -0400

    initial sync

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,4 +1,38 @@
-var path = require('path');
+const exec = require('child_process').exec;
+
+const path = require('path');
+
+const PLUGIN_ID = require('../plugin.json').id;
+
+const NPM_TARGET = process.env.npm_lifecycle_event; //eslint-disable-line no-process-env
+let mode = 'production';
+let devtool = '';
+if (NPM_TARGET === 'debug' || NPM_TARGET === 'debug:watch') {
+    mode = 'development';
+    devtool = 'source-map';
+}
+
+const plugins = [];
+if (NPM_TARGET === 'build:watch' || NPM_TARGET === 'debug:watch') {
+    plugins.push({
+        apply: (compiler) => {
+            compiler.hooks.watchRun.tap('WatchStartPlugin', () => {
+                // eslint-disable-next-line no-console
+                console.log('Change detected. Rebuilding webapp.');
+            });
+            compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
+                exec('cd .. && make deploy-from-watch', (err, stdout, stderr) => {
+                    if (stdout) {
+                        process.stdout.write(stdout);
+                    }
+                    if (stderr) {
+                        process.stderr.write(stderr);
+                    }
+                });
+            });
+        },
+    });
+}
 
 module.exports = {
     entry: [
@@ -10,50 +44,36 @@ module.exports = {
             'node_modules',
             path.resolve(__dirname),
         ],
-        extensions: ['*', '.js', '.jsx'],
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-syntax-dynamic-import',
-                        ],
-                        presets: [
-                            ['@babel/preset-env', {
-                                targets: {
-                                    chrome: 66,
-                                    firefox: 60,
-                                    edge: 42,
-                                    ie: 11,
-                                    safari: 12,
-                                },
-                                modules: false,
-                                corejs: 3,
-                                debug: false,
-                                useBuiltIns: 'usage',
-                                shippedProposals: true,
-                            }],
-                            ['@babel/preset-react', {
-                                useBuiltIns: true,
-                            }],
-                        ],
+                        cacheDirectory: true,
+
+                        // Babel configuration is in babel.config.js because jest requires it to be there.
                     },
                 },
             },
             {
-                test: /\.css$/,
+                test: /\.(scss)$/,
                 use: [
-                    {
-                        loader: 'style-loader',
-                    },
+                    'style-loader',
                     {
                         loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['node_modules/compass-mixins/lib', 'sass'],
+                            },
+                        },
                     },
                 ],
             },
@@ -65,11 +85,15 @@ module.exports = {
         'react-redux': 'ReactRedux',
         'prop-types': 'PropTypes',
         'react-bootstrap': 'ReactBootstrap',
-        'react-dom': 'ReactDOM',
+        'react-router-dom': 'ReactRouterDom',
     },
     output: {
+        devtoolNamespace: PLUGIN_ID,
         path: path.join(__dirname, '/dist'),
         publicPath: '/',
         filename: 'main.js',
     },
+    devtool,
+    mode,
+    plugins,
 };
