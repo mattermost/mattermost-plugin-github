79fa4c43a23ca6c94cdf10aca3ea776692c91fc0
Augustas Verbickas <reg1nt1z@gmail.com>
2020-08-31 19:35:40 +0300

    bump octicon version

--- webapp/src/images/icons/changes_requested.jsx
+++ webapp/src/images/icons/changes_requested.jsx
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import GitPullRequestIcon from '@primer/octicons-react';
+import {GitPullRequestIcon} from '@primer/octicons-react';
 
 export default () => (
     <GitPullRequestIcon size='small'/>
