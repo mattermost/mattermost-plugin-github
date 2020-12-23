55b4929ffc635a5a4c3c8b1f4045f4bdfc5a0386
Sera Geyran <sgeyran@gmail.com>
2020-12-24 01:29:38 +0300

    Moves response models under graphql package. Removes query builder.

--- server/plugin/internal/graphql/model/PullRequest.go
+++ server/plugin/internal/graphql/model/PullRequest.go
@@ -3,6 +3,7 @@ package model
 type PullRequest struct {
 	URL                string              `json:"url"`
 	Number             int64               `json:"number"`
+	Title              string              `json:"title,omitempty"`
 	Status             string              `json:"status"`
 	Mergeable          bool                `json:"mergeable"`
 	MergeableState     string              `json:"mergeable_state"`
--- server/plugin/internal/graphql/model/User.go
+++ server/plugin/internal/graphql/model/User.go
@@ -7,5 +7,4 @@ type User struct {
 	AvatarURL string `json:"avatar_url,omitempty"`
 	HTMLURL   string `json:"html_url,omitempty"`
 	Name      string `json:"name,omitempty"`
-	Email     string `json:"email,omitempty"`
 }
