19f615b0da0056834b00e146a9ec610916df2282
David Janda <djanda97@gmail.com>
2021-03-07 15:52:59 -0800

    Made GitHub username clickable

--- webapp/src/components/sidebar_right/github_items.jsx
+++ webapp/src/components/sidebar_right/github_items.jsx
@@ -24,9 +24,11 @@ function GithubItems(props) {
         const repoName = item.repository_url ? item.repository_url.replace(/.+\/repos\//, '') : item.repository.full_name;
 
         let userName = null;
+        let userProfile = null;
 
         if (item.user) {
             userName = item.user.login;
+            userProfile = `https://github.com/${userName}`;
         } else if (item.owner) {
             userName = item.owner.login;
         }
@@ -158,7 +160,7 @@ function GithubItems(props) {
                     style={style.subtitle}
                 >
                     {item.created_at && ('Opened ' + formatTimeSince(item.created_at) + ' ago')}
-                    {userName && ' by ' + userName}
+                    {userName && <a href={userProfile}>' by ' {userName}</a>}
                     {(item.created_at || userName) && '.'}
                     {milestone}
                     {item.reason ? (<React.Fragment>
