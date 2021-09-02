5471a3d46ac0d8be9acfce19d33dfab4c130f402
Sanjay Jaiswal <sanjay@demansol.com>
2021-09-02 08:25:35 +0530

    gofmt webhook.go

--- server/plugin/webhook.go
+++ server/plugin/webhook.go
@@ -5,13 +5,13 @@ import (
 	"crypto/hmac"
 	"crypto/sha1" //nolint:gosec // GitHub webhooks are signed using sha1 https://developer.github.com/webhooks/.
 	"encoding/hex"
+	"github.com/google/go-github/v37/github"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/microcosm-cc/bluemonday"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/google/go-github/v37/github"
-	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 func verifyWebhookSignature(secret []byte, signature string, body []byte) (bool, error) {
