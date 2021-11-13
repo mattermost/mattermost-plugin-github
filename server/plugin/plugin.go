dc277295166676d194a5e68fb6283e3797fb9702
siba <siba@ip-172-31-33-5.ap-south-1.compute.internal>
2021-11-13 05:05:46 +0000

    resolved comments

--- server/plugin/plugin.go
+++ server/plugin/plugin.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	githubTokenKey       = "_githubtoken"
-	githubOauthTokenKey  = "_githuboauthtoken"
+	githubOauthKey       = "_githubOauthKey"
 	githubUsernameKey    = "_githubusername"
 	githubPrivateRepoKey = "_githubprivate"
 
