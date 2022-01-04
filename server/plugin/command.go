fa23ea22401e5e73f5f7af0616ab29ef82570051
rinkimekari <micahwasmuth@gmail.com>
2022-01-04 11:43:12 -0500

    use list constant instead

--- server/plugin/command.go
+++ server/plugin/command.go
@@ -233,7 +233,7 @@ func (p *Plugin) handleSubscriptions(c *plugin.Context, args *model.CommandArgs,
 
 	command := parameters[0]
 
-	if command == "list" {
+	if command == list {
 		return p.handleSubscriptionsList(c, args, parameters, userInfo)
 	}
 
