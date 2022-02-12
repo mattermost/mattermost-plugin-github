ab6db80f05b7fa42625cd54990e6c31b1ad7a0c5
Ben Schumacher <ben.schumacher@mattermost.com>
2022-02-12 15:54:05 +0100

    Use username for delegate

--- server/plugin/flows.go
+++ server/plugin/flows.go
@@ -164,6 +164,7 @@ const (
 	stepCancel  flow.Name = "cancel"
 
 	keyDelegatedFrom               = "DelegatedFrom"
+	keyDelegatedTo                 = "DelgatedTo"
 	keyBaseURL                     = "BaseURL"
 	keyUsePreregisteredApplication = "UsePreregisteredApplication"
 	keyIsOAuthConfigured           = "IsOAuthConfigured"
@@ -335,15 +336,15 @@ func (fm *FlowManager) submitDelegateSelection(f *flow.Flow, submitted map[strin
 	}
 
 	return stepDelegateConfirmation, flow.State{
-		"Delegated": delegate.GetDisplayName(model.ShowNicknameFullName),
+		keyDelegatedTo: delegate.Username,
 	}, nil, nil
 }
 
 func (fm *FlowManager) stepDelegateConfirmation() flow.Step {
 	return flow.NewStep(stepDelegateConfirmation).
-		WithText("GitHub integration setup details have been sent to @{{.Delegated}}").
+		WithText("GitHub integration setup details have been sent to @{{ .DelgatedTo }}").
 		WithButton(flow.Button{
-			Name:     "Waiting for @{{ .Delegated }}...",
+			Name:     "Waiting for @{{ .DelgatedTo }}...",
 			Color:    flow.ColorDefault,
 			Disabled: true,
 		}).
@@ -352,7 +353,7 @@ func (fm *FlowManager) stepDelegateConfirmation() flow.Step {
 
 func (fm *FlowManager) stepDelegateComplete() flow.Step {
 	return flow.NewStep(stepDelegateComplete).
-		WithText("~{{.Delegated}} completed configuring the integration.").
+		WithText("@{{ .DelgatedTo }} completed configuring the integration.").
 		Next(stepDone)
 }
 
