608024fbbdaf3e1adf23586d3796c7483886c867
Ben Schumacher <ben.schumacher@mattermost.com>
2022-03-01 12:13:47 +0100

    Fix MM-42096 (#542)

--- server/plugin/webhook.go
+++ server/plugin/webhook.go
@@ -6,7 +6,7 @@ import (
 	"crypto/sha1" //nolint:gosec // GitHub webhooks are signed using sha1 https://developer.github.com/webhooks/.
 	"encoding/hex"
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 	"sync"
@@ -147,10 +147,22 @@ func (wb *WebhookBroker) Close() {
 func (p *Plugin) handleWebhook(w http.ResponseWriter, r *http.Request) {
 	config := p.getConfiguration()
 
-	signature := r.Header.Get("X-Hub-Signature")
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Bad request body", http.StatusBadRequest)
+		return
+	}
+
+	signature := r.Header.Get("X-Hub-Signature")
+	valid, err := verifyWebhookSignature([]byte(config.WebhookSecret), signature, body)
+	if err != nil {
+		p.API.LogWarn("Failed to verify webhook signature", "error", err.Error())
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	if !valid {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
 		return
 	}
 
@@ -169,17 +181,6 @@ func (p *Plugin) handleWebhook(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		p.API.LogDebug("Webhook Event Log", "event", string(bodyByte))
-	}
-	valid, err := verifyWebhookSignature([]byte(config.WebhookSecret), signature, body)
-	if err != nil {
-		p.API.LogWarn("Failed to verify webhook signature", "error", err.Error())
-		http.Error(w, "", http.StatusInternalServerError)
-		return
-	}
-
-	if !valid {
-		http.Error(w, "Not authorized", http.StatusUnauthorized)
-		return
 	}
 
 	var repo *github.Repository
