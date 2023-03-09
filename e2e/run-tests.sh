835c014a33dcf76bc1e7a1fd5505c867945e9d8d
Michael Kochell <mjkochell@gmail.com>
2023-03-09 18:23:25 +0000

    reference mm-webapp directly via tsconfig.json, and npm runn commands

--- e2e/run-tests.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s/testDir.*/testDir: '\.\.\/\.\.\/\.\.\/mattermost-plugin-github\/e2e',/g" ../../mattermost-webapp/e2e/playwright/playwright.config.ts
-npm run test-slomo --prefix ../../mattermost-webapp/e2e/playwright
