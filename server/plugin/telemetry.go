9f1a3e230eb8197aa5940fa3895d304527170415
Ben Schumacher <ben.schumacher@mattermost.com>
2023-03-21 15:58:52 +0100

    Also log errors from user events

--- server/plugin/telemetry.go
+++ server/plugin/telemetry.go
@@ -18,6 +18,13 @@ func (p *Plugin) TrackEvent(event string, properties map[string]interface{}) {
 	}
 }
 
+func (p *Plugin) TrackUserEvent(event, userID string, properties map[string]interface{}) {
+	err := p.tracker.TrackUserEvent(event, userID, properties)
+	if err != nil {
+		p.API.LogDebug("Error sending user telemetry event", "event", event, "error", err.Error())
+	}
+}
+
 func (p *Plugin) SendDailyTelemetry() {
 	config := p.getConfiguration()
 
