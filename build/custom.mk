80bb353426af6b356811794e72d4c2a54ae96d77
Jose Peso <jmpeso@gmail.com>
2023-03-28 18:33:31 +0200

    name of the rudder key for prod changed

--- build/custom.mk
+++ build/custom.mk
@@ -1,6 +1,7 @@
 # Include custom targets and environment variables here
 
-ifndef MM_RUDDER_WRITE_KEY
-	MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+# If there's no MM_RUDDER_PLUGINS_PROD, add DEV data
+ifndef MM_RUDDER_PLUGINS_PROD
+	MM_RUDDER_PLUGINS_PROD = 1d5bMvdrfWClLxgK1FvV3s4U1tg
 endif
-GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(MM_RUDDER_WRITE_KEY)"'
+GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(MM_RUDDER_PLUGINS_PROD)"'
