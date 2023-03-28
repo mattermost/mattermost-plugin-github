74bb872cc9c57e406ddbed0b143651697e752615
Jose Peso <jmpeso@gmail.com>
2023-03-28 18:37:58 +0200

    improve naming

--- build/custom.mk
+++ build/custom.mk
@@ -1,7 +1,9 @@
 # Include custom targets and environment variables here
 
 # If there's no MM_RUDDER_PLUGINS_PROD, add DEV data
-ifndef MM_RUDDER_PLUGINS_PROD
-	MM_RUDDER_PLUGINS_PROD = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+ifdef MM_RUDDER_PLUGINS_PROD
+RUDDER_WRITE_KEY = $(MM_RUDDER_PLUGINS_PROD)
 endif
-GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(MM_RUDDER_PLUGINS_PROD)"'
+
+GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(RUDDER_WRITE_KEY)"'
