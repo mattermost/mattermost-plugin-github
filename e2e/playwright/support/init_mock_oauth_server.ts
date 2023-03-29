0c8e871c9190543d63b863e0440fe6ea59076aed
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-03-29 00:03:54 -0400

    fail when oauth token is not provided

--- e2e/playwright/support/init_mock_oauth_server.ts
+++ e2e/playwright/support/init_mock_oauth_server.ts
@@ -3,7 +3,7 @@ import {ExpiryAlgorithms, makeOAuthServer} from '../mock_oauth_server/mock_oauth
 const mockOAuthAccessToken = process.env.PLUGIN_E2E_MOCK_OAUTH_TOKEN;
 if (!mockOAuthAccessToken) {
     console.error('Please provide an OAuth access token to use via env var PLUGIN_E2E_MOCK_OAUTH_TOKEN');
-    process.exit(0);
+    process.exit(1);
 }
 
 export const runOAuthServer = async () => {
