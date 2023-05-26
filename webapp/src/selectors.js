1210de00dc39f642594dc712b25a4a4e527876a3
Kshitij Katiyar <kshitij.katiyar@brightscout.com>
2023-05-26 17:37:23 +0530

    [MM-613]:Changed namings

--- webapp/src/selectors.js
+++ webapp/src/selectors.js
@@ -52,11 +52,11 @@ function mapPrsToDetails(prs, details) {
 export const getSidebarData = createSelector(
     getPluginState,
     (pluginState) => {
-        const {username, sidebarContent, reviewsDetails, yourPrsDetails, organization, rhsState} = pluginState;
+        const {username, sidebarContent, reviewDetails, yourPrDetails, organization, rhsState} = pluginState;
         return {
             username,
-            reviews: mapPrsToDetails(sidebarContent.reviews || emptyArray, reviewsDetails),
-            yourPrs: mapPrsToDetails(sidebarContent.prs || emptyArray, yourPrsDetails),
+            reviews: mapPrsToDetails(sidebarContent.reviews || emptyArray, reviewDetails),
+            yourPrs: mapPrsToDetails(sidebarContent.prs || emptyArray, yourPrDetails),
             yourAssignments: sidebarContent.assignments || emptyArray,
             unreads: sidebarContent.unreads || emptyArray,
             org: organization,
