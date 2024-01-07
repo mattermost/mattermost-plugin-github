249cffc257dd7014984fd27e0de6516ab87059a0
Catalin Patulea <cronos586@gmail.com>
2024-01-07 14:19:45 +0200

    [GH-719] Pushed commits events: add option to show Author instead of Committer.

--- server/plugin/configuration.go
+++ server/plugin/configuration.go
@@ -38,6 +38,7 @@ type Configuration struct {
 	EnableCodePreview           string `json:"enablecodepreview"`
 	EnableWebhookEventLogging   bool   `json:"enablewebhookeventlogging"`
 	UsePreregisteredApplication bool   `json:"usepreregisteredapplication"`
+	PushedCommitsAuthor         bool   `json:"pushedcommitsauthor"`
 }
 
 func (c *Configuration) ToMap() (map[string]interface{}, error) {
