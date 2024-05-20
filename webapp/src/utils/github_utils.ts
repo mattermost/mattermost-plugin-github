802c3601145e94328f33ad03325f54d0752fc96b
Sn-Kinos <dony435@naver.com>
2024-05-20 10:24:31 +0900

    make it ts and readable

--- webapp/src/utils/github_utils.ts
+++ webapp/src/utils/github_utils.ts
@@ -1,7 +1,7 @@
-export function isUrlCanPreview(url) {
+export function isUrlCanPreview(url: string) {
     if (url.includes('github.com/') && url.split('github.com/')[1]) {
         const [owner, repo, type, number] = url.split('github.com/')[1].split('/');
-        return !(!owner | !repo | !type | !number);
+        return Boolean(owner && repo && type && number);
     }
     return false;
 }
