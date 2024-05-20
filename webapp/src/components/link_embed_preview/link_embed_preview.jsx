6e0a69fa56ddf553bae4400eb92cff637423f94c
Sn-Kinos <dony435@naver.com>
2024-05-20 10:38:40 +0900

    early return on link_embed_preview

--- webapp/src/components/link_embed_preview/link_embed_preview.jsx
+++ webapp/src/components/link_embed_preview/link_embed_preview.jsx
@@ -86,94 +86,94 @@ export const LinkEmbedPreview = ({embed: {url}, connected}) => {
         );
     };
 
-    if (data) {
-        let date = new Date(data.created_at);
-        date = date.toDateString();
+    if (!data) {
+        return null;
+    }
+    let date = new Date(data.created_at);
+    date = date.toDateString();
 
-        let description = '';
-        if (data.body) {
-            description = data.body.substring(0, maxTicketDescriptionLength).trim();
-            if (data.body.length > maxTicketDescriptionLength) {
-                description += '...';
-            }
+    let description = '';
+    if (data.body) {
+        description = data.body.substring(0, maxTicketDescriptionLength).trim();
+        if (data.body.length > maxTicketDescriptionLength) {
+            description += '...';
         }
+    }
 
-        return (
-            <div className='github-preview github-preview--large p-4 mt-1 mb-1'>
-                <div className='header'>
-                    <span className='repo'>
-                        {data.repo}
-                    </span>
-                    {' on '}
-                    <span>{date}</span>
-                </div>
+    return (
+        <div className='github-preview github-preview--large p-4 mt-1 mb-1'>
+            <div className='header'>
+                <span className='repo'>
+                    {data.repo}
+                </span>
+                {' on '}
+                <span>{date}</span>
+            </div>
 
-                <div className='body d-flex'>
+            <div className='body d-flex'>
 
-                    {/* info */}
-                    <div className='preview-info mt-1'>
-                        <a
-                            href={url}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            <h5 className='mr-1'>
-                                { getIconElement() }
-                                {data.title}
-                            </h5>
-                            <span>{'#' + data.number}</span>
-                        </a>
-                        <div className='markdown-text mt-1 mb-1'>
-                            <ReactMarkdown linkTarget='_blank'>{description}</ReactMarkdown>
-                        </div>
+                {/* info */}
+                <div className='preview-info mt-1'>
+                    <a
+                        href={url}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <h5 className='mr-1'>
+                            { getIconElement() }
+                            {data.title}
+                        </h5>
+                        <span>{'#' + data.number}</span>
+                    </a>
+                    <div className='markdown-text mt-1 mb-1'>
+                        <ReactMarkdown linkTarget='_blank'>{description}</ReactMarkdown>
+                    </div>
 
-                        <div className='sub-info mt-1'>
-                            {/* base <- head */}
-                            {data.type === 'pull' && (
-                                <div className='sub-info-block'>
-                                    <h6 className='mt-0 mb-1'>{'Base ← Head'}</h6>
-                                    <div className='base-head'>
-                                        <span
-                                            title={data.base.ref}
-                                            className='commit-ref'
-                                        >{data.base.ref}
-                                        </span> <span className='mx-1'>{'←'}</span>{' '}
-                                        <span
-                                            title={data.head.ref}
-                                            className='commit-ref'
-                                        >{data.head.ref}
-                                        </span>
-                                    </div>
+                    <div className='sub-info mt-1'>
+                        {/* base <- head */}
+                        {data.type === 'pull' && (
+                            <div className='sub-info-block'>
+                                <h6 className='mt-0 mb-1'>{'Base ← Head'}</h6>
+                                <div className='base-head'>
+                                    <span
+                                        title={data.base.ref}
+                                        className='commit-ref'
+                                    >{data.base.ref}
+                                    </span> <span className='mx-1'>{'←'}</span>{' '}
+                                    <span
+                                        title={data.head.ref}
+                                        className='commit-ref'
+                                    >{data.head.ref}
+                                    </span>
                                 </div>
-                            )}
+                            </div>
+                        )}
 
-                            {/* Labels */}
-                            {data.labels && data.labels.length > 0 && (
-                                <div className='sub-info-block'>
-                                    <h6 className='mt-0 mb-1'>{'Labels'}</h6>
-                                    <div className='labels'>
-                                        {data.labels.map((label, idx) => {
-                                            return (
-                                                <span
-                                                    key={`${label.name}-${idx}`}
-                                                    className='label'
-                                                    title={label.description}
-                                                    style={{backgroundColor: '#' + label.color, color: getLabelFontColor(label.color)}}
-                                                >
-                                                    <span>{label.name}</span>
-                                                </span>
-                                            );
-                                        })}
-                                    </div>
+                        {/* Labels */}
+                        {data.labels && data.labels.length > 0 && (
+                            <div className='sub-info-block'>
+                                <h6 className='mt-0 mb-1'>{'Labels'}</h6>
+                                <div className='labels'>
+                                    {data.labels.map((label, idx) => {
+                                        return (
+                                            <span
+                                                key={`${label.name}-${idx}`}
+                                                className='label'
+                                                title={label.description}
+                                                style={{backgroundColor: '#' + label.color, color: getLabelFontColor(label.color)}}
+                                            >
+                                                <span>{label.name}</span>
+                                            </span>
+                                        );
+                                    })}
                                 </div>
-                            )}
-                        </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
-        );
-    }
-    return null;
+        </div>
+    );
 };
 
 LinkEmbedPreview.propTypes = {
