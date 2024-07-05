136221f5b067e00b5d20182b55ba256c62235c0f
raghavaggarwal2308 <raghav.aggarwal@brightscout.com>
2024-07-05 15:24:23 +0530

    [MM-556] Review fixes

--- webapp/src/components/github_issue/index.tsx
+++ webapp/src/components/github_issue/index.tsx
@@ -28,20 +28,17 @@ const GithubIssue = ({theme, post}: GithubIssueProps) => {
     };
 
     const content = (
-        <div>
+        <div style={style.button_container}>
             <button
-                style={{...style.button, ...style.other_buttons}}
                 className='btn btn-primary'
                 onClick={() => dispatch(openCreateCommentOnIssueModal(issue))}
             >{'Comment'}</button>
             <button
-                style={{...style.button, ...style.other_buttons}}
-                className='btn btn-primary'
+                className='btn btn-tertiary'
                 onClick={() => dispatch(openCreateOrUpdateIssueModal(issue))}
             >{'Edit'}</button>
             <button
-                style={{...style.button, ...{...postProps.status === 'Close' ? style.close_or_reopen_button : style.other_buttons}}}
-                className='btn btn-primary'
+                className='btn btn-tertiary'
                 onClick={() => dispatch(openCloseOrReopenIssueModal(issue))}
             >{postProps.status}</button>
         </div>
@@ -49,7 +46,7 @@ const GithubIssue = ({theme, post}: GithubIssueProps) => {
 
     if (postProps.assignees?.length) {
         assignees = (
-            <div style={style.assignees_and_labels}>
+            <div style={style.assignee}>
                 <b>{'Assignees'}</b>
                 <div>
                     {postProps.assignees.map((assignee: string, index: number) => (
@@ -62,7 +59,7 @@ const GithubIssue = ({theme, post}: GithubIssueProps) => {
 
     if (postProps.labels?.length) {
         labels = (
-            <div style={style.assignees_and_labels}>
+            <div>
                 <b>{'Labels'}</b>
                 <div>
                     {postProps.labels.map((label: string, index: number) => (
@@ -75,39 +72,39 @@ const GithubIssue = ({theme, post}: GithubIssueProps) => {
 
     return (
         <div>
-            <h5>
+            <h4>
                 <a
                     href={postProps.issue_url}
                     target='_blank'
                     rel='noopener noreferrer'
+                    style={style.issue_title}
                 >
                     {'#' + postProps.issue_number + ' ' + postProps.title}
                 </a>
-            </h5>
-            <p>{postProps.description}</p>
-            {assignees}
-            {labels}
+            </h4>
+            <p style={style.issue_description}>{postProps.description}</p>
+            <div className='d-flex display-flex'>
+                {assignees}
+                {labels}
+            </div>
             {content}
         </div>
     );
 };
 
 const getStyle = makeStyleFromTheme((theme) => ({
-    button: {
-        fontFamily: 'Open Sans',
-        fontSize: '12px',
-        fontWeight: 'bold',
-        letterSpacing: '1px',
-        lineHeight: '19px',
-        margin: '12px 12px 8px 0px',
-        borderRadius: '4px',
-        color: theme.buttonColor,
+    button_container: {
+        margin: '16px 0 8px 0',
     },
-    close_or_reopen_button: {
-        backgroundColor: theme.errorTextColor,
+    issue_description: {
+        marginBottom: '10px',
     },
-    other_buttons: {
-        backgroundColor: theme.buttonBg,
+    assignee: {
+        marginRight: '20px',
+    },
+    issue_title: {
+        fontFamily: 'Metropolis',
+        fontWeight: 600,
     },
     assignees_and_labels: {
         display: 'inline-block',
