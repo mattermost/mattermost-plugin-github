1a98f84648cc9ae6438b9efeb29c17965c5b766d
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-09-18 20:06:40 +0530

    [MM-770]: fixed lint issues

--- webapp/src/components/modals/create_issue/create_issue.test.jsx
+++ webapp/src/components/modals/create_issue/create_issue.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
+
 import CreateIssueModal from './create_issue';
 
 jest.mock('utils/user_utils', () => ({
@@ -15,64 +16,65 @@ describe('CreateIssueModal', () => {
         channelId: '',
         theme: {
             centerChannelColor: '#000',
-            centerChannelBg: '#fff'
+            centerChannelBg: '#fff',
         },
         visible: true,
     };
 
     it('should render correctly with default props', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
         expect(wrapper).toMatchSnapshot();
     });
 
     it('should call close prop when handleClose is called', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
         wrapper.instance().handleClose();
         expect(defaultProps.close).toHaveBeenCalled();
     });
 
     it('should call create prop when form is submitted with valid data', async () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        wrapper.setState({ issueTitle: 'Test Issue' });
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        wrapper.setState({issueTitle: 'Test Issue'});
 
-        await wrapper.instance().handleCreate({ preventDefault: () => {} });
+        await wrapper.instance().handleCreate({preventDefault: jest.fn()});
         expect(defaultProps.create).toHaveBeenCalled();
     });
 
     it('should display error message when create returns an error', async () => {
+        const mockCreateFunction = jest.fn().mockResolvedValue({error: {message: 'Some error'}});
         const errorProps = {
             ...defaultProps,
-            create: jest.fn(() => Promise.resolve({ error: { message: 'Some error' } })),
+            create: mockCreateFunction,
         };
 
-        const wrapper = shallow(<CreateIssueModal {...errorProps} />);
-        wrapper.setState({ issueTitle: 'Test Issue' });
+        const wrapper = shallow(<CreateIssueModal {...errorProps}/>);
+        wrapper.setState({issueTitle: 'Test Issue'});
 
-        await wrapper.instance().handleCreate({ preventDefault: () => {} });
+        await wrapper.instance().handleCreate({preventDefault: jest.fn()});
         wrapper.update();
 
         expect(wrapper.find('.help-text.error-text').text()).toEqual('Error occurred');
     });
 
     it('should show validation error when issueTitle is empty', async () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        wrapper.setState({ issueTitle: '' });
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        wrapper.setState({issueTitle: ''});
 
-        await wrapper.instance().handleCreate({ preventDefault: () => {} });
+        await wrapper.instance().handleCreate({preventDefault: jest.fn()});
         expect(wrapper.state('issueTitleValid')).toBe(false);
         expect(wrapper.state('showErrors')).toBe(true);
     });
 
     it('should update repo state when handleRepoChange is called', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        const repo = { name: 'repo-name' };
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        const repo = {name: 'repo-name'};
 
         wrapper.instance().handleRepoChange(repo);
         expect(wrapper.state('repo')).toEqual(repo);
     });
 
     it('should update labels state when handleLabelsChange is called', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
         const labels = ['label1', 'label2'];
 
         wrapper.instance().handleLabelsChange(labels);
@@ -80,7 +82,7 @@ describe('CreateIssueModal', () => {
     });
 
     it('should update assignees state when handleAssigneesChange is called', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
         const assignees = ['user1', 'user2'];
 
         wrapper.instance().handleAssigneesChange(assignees);
@@ -88,23 +90,23 @@ describe('CreateIssueModal', () => {
     });
 
     it('should set issueDescription state when post prop is updated', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        const post = { message: 'test post' };
-        wrapper.setProps({ post });
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        const post = {message: 'test post'};
+        wrapper.setProps({post});
 
         expect(wrapper.state('issueDescription')).toEqual(post.message);
     });
 
     it('should not display attribute selectors when repo does not have push permissions', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        wrapper.setState({ repo: { name: 'repo-name', permissions: { push: false } } });
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        wrapper.setState({repo: {name: 'repo-name', permissions: {push: false}}});
 
         expect(wrapper.instance().renderIssueAttributeSelectors()).toBeNull();
     });
 
     it('should display attribute selectors when repo has push permissions', () => {
-        const wrapper = shallow(<CreateIssueModal {...defaultProps} />);
-        wrapper.setState({ repo: { name: 'repo-name', permissions: { push: true } } });
+        const wrapper = shallow(<CreateIssueModal {...defaultProps}/>);
+        wrapper.setState({repo: {name: 'repo-name', permissions: {push: true}}});
 
         const selectors = wrapper.instance().renderIssueAttributeSelectors();
         expect(selectors).not.toBeNull();
