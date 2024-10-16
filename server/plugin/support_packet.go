d29cdd48763dc245a0f883d253c4c81df89ff0c9
Ben Schumacher <ben.schumacher@mattermost.com>
2024-10-16 09:32:38 +0200

    Add diagnostics data to the Support Packet

---
+++ server/plugin/support_packet.go
@@ -0,0 +1,41 @@
+package plugin
+
+import (
+	"path"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/plugin"
+)
+
+type SupportPacket struct {
+	Version            string `yaml:"version"`
+	ConnectedUserCount int64  `yaml:"connected_user_count"`
+	IsOAuthConfigured  bool   `yaml:"is_oauth_configured"`
+}
+
+func (p *Plugin) GenerateSupportData(_ *plugin.Context) ([]*model.FileData, error) {
+	config := p.getConfiguration()
+
+	connectedUserCount, err := p.getConnectedUserCount()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get the number of connected users for Support Packet")
+	}
+
+	diagnostics := SupportPacket{
+		Version:            Manifest.Version,
+		ConnectedUserCount: connectedUserCount,
+		IsOAuthConfigured:  config.IsOAuthConfigured(),
+	}
+	b, err := yaml.Marshal(diagnostics)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to marshal diagnostics")
+	}
+
+	return []*model.FileData{{
+		Filename: path.Join(Manifest.Id, "diagnostics.yaml"),
+		Body:     b,
+	}}, nil
+}
