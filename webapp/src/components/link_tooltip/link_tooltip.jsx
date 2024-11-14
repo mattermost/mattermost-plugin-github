e95f21ad866715da6aba75105331be8aadcb3885
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2024-11-14 16:51:13 +0530

    [MM-401]: Added pr/issue opened by in tooltip modal (#833)
    * [MM-401]: added pr/issue opened by in tooltip modal
    
    * [MM-401]: fixed lint

--- webapp/src/components/link_tooltip/link_tooltip.jsx
+++ webapp/src/components/link_tooltip/link_tooltip.jsx
@@ -127,6 +127,12 @@ export const LinkTooltip = ({href, connected, show, theme}) => {
                                 <h5 className='mr-1'>{data.title}</h5>
                                 <span>{'#' + data.number}</span>
                             </a>
+                            {data?.user?.login && (
+                                <p className='opened-by'>
+                                    {'Opened by '}
+                                    <a href={`https://github.com/${data.user.login}`}>{data.user.login}</a>
+                                </p>
+                            )}
                             <div className='markdown-text mt-1 mb-1'>
                                 <ReactMarkdown linkTarget='_blank'>{description}</ReactMarkdown>
                             </div>
