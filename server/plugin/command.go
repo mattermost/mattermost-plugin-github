505e5399f93e4ccc93ba517575b0cc34faf98795
Ayush Thakur <100013900+ayusht2810@users.noreply.github.com>
2024-11-14 13:34:44 +0530

    [MM-356] Add feature to subscribe to release and workflow events (#765)
    * [MM-356] Add feature to publish workflow failure events
    
    * [MM-356] Update webhook event name
    
    * [MM-356] Update go-github package and included workflow name in the template
    
    * [MM-356]: added releases to subscription feature list
    
    * [MM-356]: fixed ci
    
    * Updated package-lock file
    
    * [MM-356]: Added success testcase for workflow notification and reverted package-lock changes
    
    * [MM-356]: reverted the package-lock changes
    
    * [MM-356]: minor updates in template_test.go file
    
    ---------
    
    Co-authored-by: kshitij katiyar <kshitij.katiyar@brightscout.com>
    Co-authored-by: raghavaggarwal2308 <raghav.aggarwal@brightscout.com>

--- server/plugin/command.go
+++ server/plugin/command.go
@@ -27,6 +27,8 @@ const (
 	featurePullReviews        = "pull_reviews"
 	featureStars              = "stars"
 	featureReleases           = "releases"
+	featureWorkflowFailure    = "workflow_failure"
+	featureWorkflowSuccess    = "workflow_success"
 	featureDiscussions        = "discussions"
 	featureDiscussionComments = "discussion_comments"
 )
@@ -48,6 +50,8 @@ var validFeatures = map[string]bool{
 	featurePullReviews:        true,
 	featureStars:              true,
 	featureReleases:           true,
+	featureWorkflowFailure:    true,
+	featureWorkflowSuccess:    true,
 	featureDiscussions:        true,
 	featureDiscussionComments: true,
 }
@@ -904,7 +908,7 @@ func getAutocompleteData(config *Configuration) *model.AutocompleteData {
 
 	subscriptionsAdd := model.NewAutocompleteData("add", "[owner/repo] [features] [flags]", "Subscribe the current channel to receive notifications about opened pull requests and issues for an organization or repository. [features] and [flags] are optional arguments")
 	subscriptionsAdd.AddTextArgument("Owner/repo to subscribe to", "[owner/repo]", "")
-	subscriptionsAdd.AddNamedTextArgument("features", "Comma-delimited list of one or more of: issues, pulls, pulls_merged, pulls_created, pushes, creates, deletes, issue_creations, issue_comments, pull_reviews, releases, discussions, discussion_comments, label:\"<labelname>\". Defaults to pulls,issues,creates,deletes", "", `/[^,-\s]+(,[^,-\s]+)*/`, false)
+	subscriptionsAdd.AddNamedTextArgument("features", "Comma-delimited list of one or more of: issues, pulls, pulls_merged, pulls_created, pushes, creates, deletes, issue_creations, issue_comments, pull_reviews, releases, workflow_success, workflow_failure, discussions, discussion_comments, label:\"<labelname>\". Defaults to pulls,issues,creates,deletes", "", `/[^,-\s]+(,[^,-\s]+)*/`, false)
 
 	if config.GitHubOrg != "" {
 		subscriptionsAdd.AddNamedStaticListArgument("exclude-org-member", "Events triggered by organization members will not be delivered (the organization config should be set, otherwise this flag has not effect)", false, []model.AutocompleteListItem{
