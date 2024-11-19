eb10d0a57d805d5a027dc32e500ff9f4e8a1058b
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-11-19 16:27:20 +0530

    [MM-945]: Converting user_attribute component and client files to TS

---
+++ webapp/src/components/user_attribute/index.ts
@@ -0,0 +1,53 @@
+
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {connect} from 'react-redux';
+import {Dispatch, bindActionCreators} from 'redux';
+
+import {UserProfile} from 'mattermost-redux/types/users';
+
+import {getPluginState} from '../../selectors';
+
+import {GlobalState} from '../../types/store';
+
+import {getGitHubUser} from '../../actions';
+
+import {UserAttribute} from './user_attribute';
+
+type OwnProps = {
+    user: UserProfile;
+};
+
+type StateProps = {
+    id: string;
+    username?: string;
+    enterpriseURL: string;
+}
+
+function mapStateToProps(state: GlobalState, ownProps: OwnProps): StateProps {
+    const mmUserId = ownProps.user ? ownProps.user.id : '';
+
+    const pluginState = getPluginState(state);
+    const githubUser = pluginState.githubUsers[mmUserId];
+
+    return {
+        id: mmUserId,
+        username: githubUser?.username,
+        enterpriseURL: pluginState.enterpriseURL,
+    };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        actions: bindActionCreators({
+            getGitHubUser,
+        }, dispatch),
+    };
+}
+
+type DispatchProps = ReturnType<typeof mapDispatchToProps>;
+
+export type Props = OwnProps & StateProps & DispatchProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserAttribute);
