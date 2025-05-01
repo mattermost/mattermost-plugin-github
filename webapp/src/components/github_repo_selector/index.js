c59c27f19b5bbd3ac201762e2f0ab7c01db53b9a
kshitij katiyar <kshitij.katiyar@brightscout.com>
2025-05-01 15:59:16 +0530

    resolved manifest import in gihub repo selector

--- webapp/src/components/github_repo_selector/index.js
+++ webapp/src/components/github_repo_selector/index.js
@@ -4,7 +4,7 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import manifest from 'manifest';
+import manifest from '@/manifest';
 
 import {getReposByOrg, getOrgs} from '../../actions';
 
