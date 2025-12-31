b033626103a9a2e92a3e1a38412c68f678f990be
Eva Sarafianou <eva.sarafianou@gmail.com>
2025-12-31 11:21:47 +0200

    Updates mattermost/server/public & removes telemetry

--- build/custom.mk
+++ build/custom.mk
@@ -1,13 +1,5 @@
 # Include custom targets and environment variables here
 
-# If there's no MM_RUDDER_PLUGINS_PROD, add DEV data
-RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
-ifdef MM_RUDDER_PLUGINS_PROD
-RUDDER_WRITE_KEY = $(MM_RUDDER_PLUGINS_PROD)
-endif
-
-GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost/server/public/pluginapi/experimental/telemetry.rudderWriteKey=$(RUDDER_WRITE_KEY)"'
-
 ifdef PLUGIN_E2E_MOCK_OAUTH_SERVER_URL
 	GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-github/server/plugin.testOAuthServerURL=$(PLUGIN_E2E_MOCK_OAUTH_SERVER_URL)"'
 endif
